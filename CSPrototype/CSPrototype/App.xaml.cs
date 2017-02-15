f4b48c2e42daf39ac926364c1582c85ec7ff261e
Wojciech Witek <wojtekwitek@gmail.com>
2017-02-15 05:32:22 +0100

    Added HomePage with CocosSharpView in xaml

--- CSPrototype/CSPrototype/App.xaml.cs
+++ CSPrototype/CSPrototype/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSPrototype.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,7 @@ namespace CSPrototype
         public App()
         {
             InitializeComponent();
-
-            MainPage = new CSPrototype.MainPage();
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
